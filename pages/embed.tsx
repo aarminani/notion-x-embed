054e3d91f8906141a471e04d3f5c62c21a2578ef
aarmani <bassettisaiah0@gmail.com>
2026-02-27 20:01:52 -0500

    force twitter init

--- pages/embed.tsx
+++ pages/embed.tsx
@@ -1,22 +1,35 @@
 import { useEffect } from "react";
 
 export default function Embed() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://platform.twitter.com/widgets.js";
-        script.async = true;
-        document.body.appendChild(script);
-    }, []);
+  useEffect(() => {
+    const loadTwitter = async () => {
+      if ((window as any).twttr) {
+        (window as any).twttr.widgets.load();
+        return;
+      }
 
-    return (
-        <div style={{ padding: 0, margin: 0 }}>
-            <a
-                className="twitter-timeline"
-                data-tweet-limit="1"
-                href="https://twitter.com/tekken"
-            >
-                Loading Tweets...
-            </a>
-        </div>
-    );
+      const script = document.createElement("script");
+      script.src = "https://platform.twitter.com/widgets.js";
+      script.async = true;
+      script.onload = () => {
+        (window as any).twttr.widgets.load();
+      };
+      document.body.appendChild(script);
+    };
+
+    loadTwitter();
+  }, []);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <a
+        className="twitter-timeline"
+        data-tweet-limit="1"
+        data-width="550"
+        href="https://twitter.com/tekken"
+      >
+        Loading Tweets...
+      </a>
+    </div>
+  );
 }
